44cd443dafb35841b70096c4a302f72221826e79
NOELADAVILA <00001090063519sp@al.educacao.sp.gov.br>
2024-11-18 06:37:35 -0300

    Add files via upload

---
+++ Perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'política',
+   ' Qual é o atual presidente do Estados Unidos?',
+    'O atual presidente do Estados unidos é Donald Trump'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Peru?',
+    'A capital do Peru Lima'
+)
+
+criaCartao(
+    'Matemática',
+    'quanto é 30% de 120?',
+    ' 30% de 120 é 36'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a formula de Bhaskhara?',
+    ' a formula de Bhaskhara é b±√(b²-4ac))/(2a)'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano terminou a Segunda Guerra Mundial?',
+    'Terminou em 2 de Setembro de 1945'
+)
+
+
+criaCartao(
+    'pessoal',
+    'O quão legal é o professor careca Márcio Mandalho?',
+    '100%'
+)
